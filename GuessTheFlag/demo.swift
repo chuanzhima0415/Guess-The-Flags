b73244ce8e2c4f3707ad6f6fff286e0cfa4ed95b
chanzyma <chuanzhima0415@gmail.com>
2026-04-10 23:09:17 +0800

    update struct c
    (cherry picked from commit efb46664e4a5f6c197babf27fd83e291181f8b5f)
    
    # Conflicts:
    #	GuessTheFlag/demo.swift

---
+++ GuessTheFlag/demo.swift
@@ -0,0 +1,30 @@
+//
+//  demo.swift
+//  GuessTheFlag
+//
+//  Created by Chanzy Ma on 2026/4/10.
+//
+
+import Foundation
+
+struct A {
+    var a: Int = 0
+    var b: Int = 0
+    var c: Int = 0
+	var d: Int = 0
+	var e: Int = 0
+	var f: Int = 0
+}
+
+
+struct B {
+    var a: Int = 0
+    var b: Int = 0
+    var c = 0
+    var d = 0
+    var e = 0
+}
+
+struct D {
+    var a: Int = 0
+}
